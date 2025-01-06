2a1a26a776090457791d1ea714c14555883d0bc5
marqs <mhiienka@niksula.hut.fi>
2025-01-07 01:42:14 +0200

    add driver for si2177

--- si2177/si2177.c
+++ si2177/si2177.c
@@ -21,42 +21,117 @@
 #include <stdint.h>
 #include <string.h>
 #include "si2177.h"
+#include "sysconfig.h"
 #include "i2c_opencores.h"
 
 const si2177_config si2177_cfg_default = {
-    .tmp = 1
+    .tv_std = 0,
+    .ch_idx = 0
 };
 
-void si2177_writereg(si2177_dev *dev, uint8_t regaddr, uint8_t data) {
-    I2C_start(dev->i2cm_base, dev->i2c_addr, 0);
-    I2C_write(dev->i2cm_base, regaddr, 0);
-    I2C_write(dev->i2cm_base, data, 1);
-}
-
-uint8_t si2177_readreg(si2177_dev *dev, uint8_t regaddr) {
-    //Phase 1
-    I2C_start(dev->i2cm_base, dev->i2c_addr, 0);
-    I2C_write(dev->i2cm_base, regaddr, 0);
-
-    //Phase 2
-    I2C_start(dev->i2cm_base, dev->i2c_addr, 1);
-    return I2C_read(dev->i2cm_base,1);
+void si2177_get_default_cfg(si2177_config *cfg) {
+    memcpy(cfg, &si2177_cfg_default, sizeof(si2177_config));
 }
 
 int si2177_init(si2177_dev *dev) {
+    unsigned char retval;
+
     memcpy(&dev->cfg, &si2177_cfg_default, sizeof(si2177_config));
+    memset(&dev->ch, 0, sizeof(si2177_channel));
+    dev->powered_on = 0;
 
-    /*if (si2177_readreg(dev, 0x11) != 0x43)
-        return -1;*/
+    retval = Si2177_L1_API_Init(&dev->l1_ctx, dev->i2c_addr);
+    if (retval != NO_Si2177_ERROR) {
+        printf("L1 Init failed\n");
+        return -1;
+    }
 
-    /*for (int i=0; i<=0x13; i++)
-        printf("SI2177 0x%.2x 0x%.2x\n", i, si2177_readreg(dev, i));*/
+    retval = Si2177_Init(&dev->l1_ctx);
+    if (retval != NO_Si2177_ERROR) {
+        printf("Init failed\n");
+        return -1;
+    }
 
-    //si2177_enable_power(dev, 0);
+    dev->powered_on = 1;
 
     return 0;
 }
 
-void si2177_enable_power(si2177_dev *dev, int enable) {
+void si2177_update_config(si2177_dev *dev, si2177_config *cfg) {
+    if (dev->powered_on) {
+        if (memcmp(&dev->ch, &cfg->chlist[cfg->ch_idx], sizeof(si2177_channel)))
+            si2177_tune(dev, &cfg->chlist[cfg->ch_idx]);
 
+        memcpy(&dev->cfg, cfg, sizeof(si2177_config));
+        memcpy(&dev->ch, &cfg->chlist[cfg->ch_idx], sizeof(si2177_channel));
+    }
+}
+
+int si2177_channelscan(si2177_dev *dev, si2177_channel *chlist) {
+    int retval, i;
+
+    if (!dev->powered_on)
+        return -1;
+
+    printf("Channelscan %s - atv_rsq_rssi_threshold.lo: %u, atv_rsq_rssi_threshold.hi: %u, atv_rsq_snr_threshold.lo: %u, atv_rsq_snr_threshold.hi: %u\n",
+            dev->cfg.tv_std ? "PAL" : "NTSC",
+            dev->l1_ctx.prop->atv_rsq_rssi_threshold.lo, dev->l1_ctx.prop->atv_rsq_rssi_threshold.hi, dev->l1_ctx.prop->atv_rsq_snr_threshold.lo, dev->l1_ctx.prop->atv_rsq_snr_threshold.hi);
+
+    if (dev->cfg.tv_std == 0) {
+        retval = Si2177_ATV_Channel_Scan_M(&dev->l1_ctx,Si2177_TUNER_TUNE_FREQ_CMD_FREQ_MIN,Si2177_TUNER_TUNE_FREQ_CMD_FREQ_MAX,
+                                dev->l1_ctx.prop->atv_rsq_rssi_threshold.lo,
+                                dev->l1_ctx.prop->atv_rsq_rssi_threshold.hi,
+                                dev->l1_ctx.prop->atv_rsq_snr_threshold.lo,
+                                dev->l1_ctx.prop->atv_rsq_snr_threshold.hi);
+    } else {
+        retval = Si2177_ATV_Channel_Scan_PAL(&dev->l1_ctx,Si2177_TUNER_TUNE_FREQ_CMD_FREQ_MIN,Si2177_TUNER_TUNE_FREQ_CMD_FREQ_MAX,
+                                dev->l1_ctx.prop->atv_rsq_rssi_threshold.lo,
+                                dev->l1_ctx.prop->atv_rsq_rssi_threshold.hi,
+                                dev->l1_ctx.prop->atv_rsq_snr_threshold.lo,
+                                dev->l1_ctx.prop->atv_rsq_snr_threshold.hi);
+    }
+
+    if (retval != NO_Si2177_ERROR) {
+        printf("Scan failed\n");
+        return -1;
+    }
+
+    printf("%u Channels Found \n", dev->l1_ctx.ChannelListSize);
+    printf("Num\tType\tChannel \n");
+    for (i=0; i < dev->l1_ctx.ChannelListSize; i++) {
+        printf("%d\t%s\t%ld\n",i, dev->l1_ctx.ChannelType[i], dev->l1_ctx.ChannelList[i]);
+        chlist[i].freq = dev->l1_ctx.ChannelList[i];
+        chlist[i].tv_system = (dev->l1_ctx.ChannelType[i][0] == 'M') ? Si2177_ATV_VIDEO_MODE_PROP_VIDEO_SYS_M : Si2177_ATV_VIDEO_MODE_PROP_VIDEO_SYS_B;
+    }
+
+    // Enforce re-tune of selected channel after scan
+    memset(&dev->ch, 0, sizeof(si2177_channel));
+
+    return dev->l1_ctx.ChannelListSize;
+}
+
+int si2177_tune(si2177_dev *dev, si2177_channel *ch) {
+    int retval;
+
+    if (!dev->powered_on) {
+        return -1;
+    } else if (!ch->freq) {
+        printf("Invalid channel\n");
+        return -1;
+    }
+
+    printf("Tuning to system %u, freq %lu\n", ch->tv_system, ch->freq);
+
+    retval = Si2177_ATVTune(&dev->l1_ctx,
+                             ch->freq,
+                             ch->tv_system,
+                             Si2177_ATV_VIDEO_MODE_PROP_COLOR_PAL_NTSC,
+                             dev->l1_ctx.prop->atv_video_mode.invert_signal);
+
+    if (retval != NO_Si2177_ERROR) {
+        printf("Tune failed\n");
+        return -1;
+    }
+
+    return 0;
 }
--- si2177/si2177.h
+++ si2177/si2177.h
@@ -23,10 +23,22 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "sysconfig.h"
-#include "si2177_regs.h"
+#include "Silabs_L0_API.h"
+#include "Si2177_Commands.h"
+#include "Si2177_Properties.h"
+#include "Si2177_L1_API.h"
+#include "Si2177_L2_API.h"
+#include "Si2177_typedefs.h"
 
 typedef struct {
-    uint8_t tmp;
+    uint32_t freq;
+    uint8_t tv_system;
+} si2177_channel;
+
+typedef struct {
+    uint8_t tv_std;
+    uint8_t ch_idx;
+    si2177_channel chlist[MAX_POSSIBLE_CHANNELS];
 } si2177_config;
 
 typedef struct {
@@ -34,10 +46,18 @@ typedef struct {
     uint8_t i2c_addr;
     uint8_t powered_on;
     si2177_config cfg;
+    L1_Si2177_Context l1_ctx;
+    si2177_channel ch;
 } si2177_dev;
 
 int si2177_init(si2177_dev *dev);
 
-void si2177_enable_power(si2177_dev *dev, int enable);
+void si2177_get_default_cfg(si2177_config *cfg);
+
+void si2177_update_config(si2177_dev *dev, si2177_config *cfg);
+
+int si2177_channelscan(si2177_dev *dev, si2177_channel *chlist);
+
+int si2177_tune(si2177_dev *dev, si2177_channel *ch);
 
 #endif /* SI2177_H_ */
